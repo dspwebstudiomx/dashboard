1256ce945efbbdccbddf409a38182e2abfde02bd
Daniel Perez <dspwebstudio@gmail.com>
2025-05-05 20:02:16 -0600

    feat: Mejorar el diseño del modal de clientes con nuevos estilos y ajustes en la interfaz

--- src/pages/ClientsPage/ClientsModal.jsx
+++ src/pages/ClientsPage/ClientsModal.jsx
@@ -9,11 +9,11 @@ import {
   FaAddressCard,
   FaGlobe,
   FaFacebook,
-  FaTwitter,
   FaInstagram,
   FaLinkedin,
   FaFileAlt,
 } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
 
 const ClientsModal = ({
   isOpen,
@@ -101,183 +101,183 @@ const ClientsModal = ({
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-gray-800 bg-opacity-50 flex items-center justify-center z-50">
-      <div className="bg-white dark:bg-gray-900 rounded-lg shadow-lg w-full max-w-2xl p-6">
-        <h2 className="text-xl font-bold text-gray-800 dark:text-gray-100 mb-4">
+    <div className="fixed inset-0 bg-blue-900 bg-opacity-50 flex items-center justify-center z-50">
+      <div className="bg-white rounded-lg shadow-lg w-full max-w-2/3 p-12 border-4 border-blue-400 dark:bg-gray-800 dark:border-gray-700">
+        <h2 className="text-2xl font-bold text-gray-800 dark:text-gray-100 mb-12">
           {isEditing ? "Editar Cliente" : "Agregar Cliente"}
         </h2>
-        <form onSubmit={handleSubmit} className="space-y-4">
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-            <div className="form-group flex items-center gap-2">
-              <FaUser className="text-gray-500 dark:text-gray-300" />
+        <form onSubmit={handleSubmit} className="space-y-4 ">
+          <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-8 ">
+            <div className="form-group flex items-center gap-4">
+              <FaUser className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="fullName"
                 value={formData.fullName || ""}
                 onChange={handleChange}
                 placeholder="Nombre(s)"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300 focus:border-blue-700 focus:border-2 focus:outline-none"
                 required
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaUser className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaUser className="text-blue-900 text-2xl dark:text-gray-300" />
               <input
                 type="text"
                 name="lastName"
                 value={formData.lastName}
                 onChange={handleChange}
                 placeholder="Apellido Paterno"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
                 required
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaUser className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaUser className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="lastName2"
                 value={formData.lastName2}
                 onChange={handleChange}
                 placeholder="Apellido Materno"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaEnvelope className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaEnvelope className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="email"
                 name="email"
                 value={formData.email}
                 onChange={handleChange}
                 placeholder="Correo electrónico"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
                 required
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaPhone className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaPhone className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="tel"
                 name="phoneNumber"
                 value={formData.phoneNumber}
                 onChange={handleChange}
                 placeholder="Teléfono"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaAddressCard className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaAddressCard className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="address"
                 value={formData.address}
                 onChange={handleChange}
                 placeholder="Dirección"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaBuilding className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaBuilding className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="company"
                 value={formData.company}
                 onChange={handleChange}
                 placeholder="Empresa"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaFileAlt className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaFileAlt className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="project"
                 value={formData.project}
                 onChange={handleChange}
                 placeholder="Proyecto"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaFileAlt className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaFileAlt className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="rfc"
                 value={formData.rfc}
                 onChange={handleChange}
                 placeholder="RFC"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaFileAlt className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaFileAlt className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="text"
                 name="curp"
                 value={formData.curp}
                 onChange={handleChange}
                 placeholder="CURP"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaGlobe className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaGlobe className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="url"
                 name="website"
                 value={formData.website}
                 onChange={handleChange}
                 placeholder="Sitio web"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaFacebook className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaFacebook className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="url"
                 name="facebook"
                 value={formData.facebook}
                 onChange={handleChange}
                 placeholder="Facebook"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaTwitter className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaXTwitter className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="url"
                 name="twitter"
                 value={formData.twitter}
                 onChange={handleChange}
                 placeholder="Twitter"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaInstagram className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaInstagram className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="url"
                 name="instagram"
                 value={formData.instagram}
                 onChange={handleChange}
                 placeholder="Instagram"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
-            <div className="form-group flex items-center gap-2">
-              <FaLinkedin className="text-gray-500 dark:text-gray-300" />
+            <div className="form-group flex items-center gap-4">
+              <FaLinkedin className="text-blue-900 text-2xl  dark:text-gray-300" />
               <input
                 type="url"
                 name="linkedin"
                 value={formData.linkedin}
                 onChange={handleChange}
                 placeholder="LinkedIn"
-                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300"
+                className="flex-1 p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300  focus:border-blue-700 focus:border-2 focus:outline-none"
               />
             </div>
           </div>
-          <div className="form-actions flex justify-end gap-4">
+          <div className="form-actions flex justify-end ">
             <button
               type="button"
               onClick={onClose}
