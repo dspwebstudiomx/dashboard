35017eb304fd35a78b4fe48583b3d517261c0f65
Daniel Perez <dspwebstudio@gmail.com>
2025-05-07 11:13:28 -0600

    feat: Añadir componente Footer con información de derechos reservados y desarrollador

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-blue-800 text-white text-center mt-auto fixed bottom-0 w-full h-auto p-5 flex-col justify-evenly items-center p">
+      <p className="text-sm mb-3">
+        © 2025 dspwebstudio - Todos los derechos reservados.
+      </p>
+      <div className="flex flex-col md:items-start">
+        <p className="text-sm">Desarrollado por DSP Web Studio</p>
+        <p className="text-sm">Versión 1.0.0</p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
