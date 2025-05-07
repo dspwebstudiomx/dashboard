bdaaeedc57c61aaf97c2584d181ba313718dcece
Daniel Perez <dspwebstudio@gmail.com>
2025-05-06 19:10:54 -0600

    feat: Cambiar el color de fondo del modal a azul para mejorar la consistencia del diseño

--- src/pages/TasksPage/TasksPage.jsx
+++ src/pages/TasksPage/TasksPage.jsx
@@ -160,7 +160,7 @@ const TasksPage = () => {
 
       {/* Modal */}
       {isModalOpen && (
-        <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
+        <div className="fixed inset-0 bg-blue-900 bg-opacity-50 flex items-center justify-center z-50">
           <div className="bg-white rounded-lg shadow-lg p-8 md:py-8 w-auto max-h-screen overflow-y-auto">
             <div className="flex justify-end items-center px-4 w-full">
               <button
