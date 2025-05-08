dc2417b0b701e46bdc8a626ac07fc000cd5f900f
Daniel Perez <dspwebstudio@gmail.com>
2025-05-07 20:46:02 -0600

    feat: Agregar contenido y estilo a la página de cotizaciones para mejorar la presentación

--- src/pages/QuotesPage/QuotesPage.jsx
+++ src/pages/QuotesPage/QuotesPage.jsx
@@ -2,7 +2,27 @@ import React from "react";
 import DashboardTemplate from "../../templates/DashboardTemplate";
 
 const QuotesPage = () => {
-  return <DashboardTemplate>QuotesPage</DashboardTemplate>;
+  return (
+    <DashboardTemplate>
+      <div className="flex flex-col items-center justify-center gap-8 min-h-[69vh] text-2xl font-bold text-gray-800 dark:text-white bg-white rounded-2xl shadow-lg p-18">
+        <h1>Cotizaciones</h1>
+        <p className="text-base text-gray-600 dark:text-gray-400 font-normal">
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eveniet
+          ducimus nesciunt quibusdam amet sit alias excepturi accusantium, ipsa
+          obcaecati enim quod dolorum nihil saepe rem voluptatem aliquam
+          mollitia facere? Laborum voluptatum doloremque veritatis, id totam
+          harum iure unde laudantium distinctio vitae ab. Sit iusto laboriosam
+          maxime aperiam error. Corporis odit pariatur vel suscipit natus! Sed
+          repellendus iste labore ipsa animi voluptas perferendis, illo
+          dignissimos tempora voluptatum pariatur unde nemo mollitia soluta
+          fugit quae quaerat veritatis dolores doloremque numquam quos
+          repudiandae distinctio. Sequi vero ipsa accusantium atque totam unde
+          doloremque provident ut ipsam optio. Ipsum praesentium hic ipsam
+          suscipit delectus nostrum.
+        </p>
+      </div>
+    </DashboardTemplate>
+  );
 };
 
 export default QuotesPage;
