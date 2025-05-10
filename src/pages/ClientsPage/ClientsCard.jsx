4e02c7a20d3975fb7a50e0efc918c0bdd9bb6ebd
Daniel Perez <dspwebstudio@gmail.com>
2025-05-09 20:18:54 -0600

    feat: corregir nombre de cliente y ajustar estilos en tarjetas y lista de clientes

--- src/pages/ClientsPage/ClientsCard.jsx
+++ src/pages/ClientsPage/ClientsCard.jsx
@@ -82,9 +82,9 @@ const ClientsCard = ({ client, onClientUpdate }) => {
 
   return (
     <>
-      <article className="grid grid-cols-12 shadow-2xl rounded-lg p-12 pb-8 items-start gap-20 bg-white dark:bg-gray-800 font-semibold  last:mb-30 last:md:mb-0 xl:w-[650px] xl:h-auto">
+      <article className="grid md:grid-cols-12 shadow-2xl rounded-lg p-4 md:p-12 pb-8 items-start gap-20 bg-white dark:bg-gray-800 font-semibold  last:mb-30 last:md:mb-0 w-[100vw] xl:w-[650px] xl:h-auto">
         {/* // Lado con imagen, nombre completo y redes sociales */}
-        <div className=" col-span-3 items-center justify-between h-[80%] gap-12 my-auto ">
+        <div className=" md:col-span-3 items-center justify-between h-[80%] gap-12 my-auto ">
           <div
             id="tarjeta-clients-imagen-nombreCompleto-redesSociales"
             className="flex flex-col items-center justify-center w-full gap-6">
@@ -180,7 +180,7 @@ const ClientsCard = ({ client, onClientUpdate }) => {
           {/* Redes sociales */}
           <div
             id="tarjeta-redes-sociales"
-            className="flex justify-center items-center gap-4 mt-6 bg-amber-400">
+            className="grid grid-cols-4 items-center gap-8 mt-6 pb-8 p-4 px-8">
             {[
               {
                 href: client.website,
