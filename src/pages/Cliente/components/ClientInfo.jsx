4e95a2266d69617413f87686470d1839d6ffb26b
Daniel Perez <dspwebstudio@gmail.com>
2025-05-17 20:37:43 -0600

    feat: actualizar prioridad de tarea a baja y mejorar presentación de información del cliente

--- src/pages/Cliente/components/ClientInfo.jsx
+++ src/pages/Cliente/components/ClientInfo.jsx
@@ -26,75 +26,87 @@ const ClientInfo = ({ selectedClient }) => (
         </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdEmail className={iconClass} /> Correo Electrónico:
         </span>{" "}
         <br className="md:hidden" />
-        <span className="lowercase">
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0 lowercase">
           {selectedClient.email ? selectedClient.email : "Sin correo"}
         </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdPhone className={iconClass} /> Número Telefónico:
         </span>{" "}
         <br className="md:hidden" />
-        {selectedClient.phoneNumber
-          ? selectedClient.phoneNumber
-          : "Sin teléfono"}
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0">
+          {selectedClient.phoneNumber
+            ? selectedClient.phoneNumber
+            : "Sin teléfono"}
+        </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdHome className={iconClass} /> Dirección:
         </span>{" "}
         <br className="md:hidden" />
-        {selectedClient.address ? selectedClient.address : "Sin dirección"}
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0">
+          {selectedClient.address ? selectedClient.address : "Sin dirección"}
+        </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdFingerprint className={iconClass} /> RFC:
         </span>{" "}
         <br className="md:hidden" />
-        {selectedClient.rfc ? selectedClient.rfc : "Sin RFC"}
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0">
+          {selectedClient.rfc ? selectedClient.rfc : "Sin RFC"}
+        </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdFingerprint className={iconClass} /> CURP:
         </span>{" "}
         <br className="md:hidden" />
-        {selectedClient.curp ? selectedClient.curp : "Sin CURP"}
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0">
+          {selectedClient.curp ? selectedClient.curp : "Sin CURP"}
+        </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdDateRange className={iconClass} /> Fecha de Registro:
         </span>{" "}
         <br className="md:hidden" />
-        {new Date(selectedClient.createdAt).toLocaleDateString("es-MX", {
-          year: "numeric",
-          month: "long",
-          day: "numeric",
-        })}
+        <span className="text-blue-900 dark:text-blue-400 font-semibold ml-10 md:ml-0">
+          {new Date(selectedClient.createdAt).toLocaleDateString("es-MX", {
+            year: "numeric",
+            month: "long",
+            day: "numeric",
+          })}
+        </span>
       </p>
       <p>
-        <span className="font-semibold">
+        <span className="font-normal">
           <MdFolder className={iconClass} /> Proyectos:
         </span>{" "}
         <br className="md:hidden" />
-        {Array.isArray(selectedClient.projects) &&
-        selectedClient.projects.length > 0 ? (
-          <ul className="list-disc ml-6 mt-3">
-            {selectedClient.projects.map((proyecto, idx) => (
-              <li key={idx} className="mb-1 list-none">
-                <span>
-                  <MdFolder className={`${iconClass} text-xl`} />
-                </span>
-                {proyecto.title ? proyecto.title : JSON.stringify(proyecto)}
-              </li>
-            ))}
-          </ul>
-        ) : (
-          "Sin proyectos"
-        )}
+        <span className="text-gray-800 dark:text-gray-100 font-semibold ml-10 md:ml-0">
+          {Array.isArray(selectedClient.projects) &&
+          selectedClient.projects.length > 0 ? (
+            <ul className="list-disc ml-6 mt-3">
+              {selectedClient.projects.map((proyecto, idx) => (
+                <li key={idx} className="mb-1 list-none">
+                  <span>
+                    <MdFolder className={`${iconClass} text-xl`} />
+                  </span>
+                  {proyecto.title ? proyecto.title : JSON.stringify(proyecto)}
+                </li>
+              ))}
+            </ul>
+          ) : (
+            "Sin proyectos"
+          )}
+        </span>
       </p>
     </div>
   </div>
