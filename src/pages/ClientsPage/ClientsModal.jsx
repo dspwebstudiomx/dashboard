2ec8f63a0ab45050166c520023b8efdf73235adb
Daniel Perez <dspwebstudio@gmail.com>
2025-05-21 14:06:29 -0600

    feat: añadir componente ImageClientModal para gestionar la carga y eliminación de imágenes en ClientsModal

--- src/pages/ClientsPage/ClientsModal.jsx
+++ src/pages/ClientsPage/ClientsModal.jsx
@@ -7,6 +7,7 @@ import useClientForm from "./hooks/useClientForm";
 import ClientFormFields from "./components/ClientFormFields";
 import Button from "@components/Botones/Button";
 import { IoPersonAddOutline } from "react-icons/io5";
+import ImageClientModal from "./components/ImageClientModal";
 
 const ClientsModal = ({ isOpen, onClose, client, onClientUpdate }) => {
   const {
@@ -41,44 +42,11 @@ const ClientsModal = ({ isOpen, onClose, client, onClientUpdate }) => {
         <ClientFormFields formData={formData} handleChange={handleChange} />
 
         <div className="flex flex-col md:flex-row justify-center items-end gap-1 md:gap-4 p-2">
-          {/* Imagen */}
-          <div
-            id="image-group"
-            className="form-group flex flex-col md:flex-row items-center md:items-end gap-6 mt-12 xl:mt-6 p-2">
-            <div className="flex flex-col md:flex-row items-center gap-8">
-              <img
-                src={
-                  formData.image
-                    ? `http://localhost:5000${formData.image}`
-                    : "http://localhost:5000/uploads/avatar_placeholder_large.png"
-                }
-                alt="Imagen del cliente"
-                className="w-24 h-24 object-cover border-2 border-gray-200 dark:border-gray-700 rounded-full"
-              />
-            </div>
-            <div className="flex flex-col gap-4 justify-center items-start">
-              <label className="text-gray-700 dark:text-gray-300 flex items-start gap-4">
-                <FaRegImage className="text-blue-900 text-2xl dark:text-gray-300" />
-                <span>
-                  {formData.image ? "Cambiar imagen" : "Agregar imagen"}
-                </span>
-              </label>
-              <input
-                type="file"
-                name="image"
-                accept="image/*"
-                onChange={handleImageUpload}
-                className="p-2 border rounded-md dark:bg-gray-800 dark:border-gray-700 dark:text-gray-300 focus:border-blue-700 focus:border-2 focus:outline-none w-full md:w-[210px]"
-              />
-            </div>
-            <button
-              type="button"
-              onClick={handleRemoveImage}
-              className="flex gap-2 bg-red-500 border-2 border-red-700 hover:bg-red-400 text-white text-base w-full md:w-12 h-12 rounded-md place-items-center justify-center">
-              <RiDeleteBin6Line className="text-white text-2xl" />
-              <span className="md:hidden">Eliminar imagen</span>
-            </button>
-          </div>
+          <ImageClientModal
+            formData={formData}
+            handleImageUpload={handleImageUpload}
+            handleRemoveImage={handleRemoveImage}
+          />
           {/* Botones de acción */}
           <Button
             variant="secondary"
