bcacc8f8dda42d114907a5a4b2a415c226bd6b21
Daniel Perez <dspwebstudio@gmail.com>
2025-05-21 17:16:04 -0600

    feat: actualizar prioridad de tarea a 'Alta' en clients.json y cambiar icono en AdminActionButton

--- src/pages/Cliente/components/AdminActionButton.jsx
+++ src/pages/Cliente/components/AdminActionButton.jsx
@@ -1,13 +1,13 @@
 import Button from "@components/Botones/Button";
 import React from "react";
-import { FaEye } from "react-icons/fa6";
+import { FaRegEye } from "react-icons/fa6";
 
 const AdminActionButton = ({ onClick, text }) => {
   return (
     <Button
       variant="primary"
       text={text}
-      icon={FaEye}
+      icon={FaRegEye}
       onClick={onClick}
       type="button"
     />
