6160f22145877d8422c1e960c00e26168e20da46
Daniel Perez <dspwebstudio@gmail.com>
2025-05-25 17:16:05 -0600

    refactor: mejorar la legibilidad y consistencia del código en ProjectActionButtons y ContentProjectCard

--- src/pages/Cliente/components/ProjectActionButtons.jsx
+++ src/pages/Cliente/components/ProjectActionButtons.jsx
@@ -1,28 +1,28 @@
-import AdminActionButton from "./AdminActionButton";
-import CloseActionButton from "./CloseActionButton";
-import DeleteActionButton from "./DeleteActionButton";
-import EditActionButton from "./EditActionButton";
+import AdminActionButton from './AdminActionButton';
+import CloseActionButton from './CloseActionButton';
+// import DeleteActionButton from "./DeleteActionButton";
+// import EditActionButton from "./EditActionButton";
 
 const ProjectActionButtons = ({
-  isCompleted,
-  openAdminModal,
-  onEdit,
-  onDelete,
-  handleCompleteClick,
+	isCompleted,
+	openAdminModal,
+	// onEdit,
+	// onDelete,
+	handleCompleteClick,
 }) => {
-  if (isCompleted) return null;
-  return (
-    <>
-      <AdminActionButton onClick={openAdminModal} text="Ver Proyecto" />
-      <EditActionButton onClick={onEdit} text="Editar Proyecto" />
-      <DeleteActionButton onClick={onDelete} text="Eliminar Proyecto" />
-      <CloseActionButton
-        handleCompleteClick={handleCompleteClick}
-        text="Cerrar Proyecto"
-        onClick={handleCompleteClick}
-      />
-    </>
-  );
+	if (isCompleted) return null;
+	return (
+		<>
+			<AdminActionButton onClick={openAdminModal} text="Ver Proyecto" />
+			{/* <EditActionButton onClick={onEdit} text="Editar Proyecto" />
+      <DeleteActionButton onClick={onDelete} text="Eliminar Proyecto" /> */}
+			<CloseActionButton
+				handleCompleteClick={handleCompleteClick}
+				text="Cerrar Proyecto"
+				onClick={handleCompleteClick}
+			/>
+		</>
+	);
 };
 
 export default ProjectActionButtons;
--- src/pages/Cliente/components/ProjectCard/ContentProjectCard.jsx
+++ src/pages/Cliente/components/ProjectCard/ContentProjectCard.jsx
@@ -1,120 +1,107 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
-import ServicesProjectTag from "../ServicesProjectTag";
-import SectionsProjectTag from "../SectionsProjectTag";
-import ProjectDescriptionInfoCard from "../ProjectDescriptionInfoCard";
-import Priority from "../Priority";
-import TitleProjectCard from "@components/Texts/TitleProjectCard";
-import ProjectCardDates from "./ProjectCardDates";
-import TotalCostProyectCard from "../TotalCostProyectCard";
-import CloseProjectMessaje from "./CloseProjectMessaje";
-import { useProjectDescription } from "@hooks/useProjectDescription";
-import ProjectActionButtons from "../ProjectActionButtons";
-import AdminActionButton from "../AdminActionButton";
+import ProjectDescriptionInfoCard from '../ProjectDescriptionInfoCard';
+import Priority from '../Priority';
+import TitleProjectCard from '@components/Texts/TitleProjectCard';
+import ProjectCardDates from './ProjectCardDates';
+import TotalCostProyectCard from '../TotalCostProyectCard';
+import CloseProjectMessaje from './CloseProjectMessaje';
+import { useProjectDescription } from '@hooks/useProjectDescription';
+import ProjectActionButtons from '../ProjectActionButtons';
+import AdminActionButton from '../AdminActionButton';
 
 const ContentProjectCard = ({
-  project,
-  isCompleted,
-  onEdit,
-  onDelete,
-  handleCompleteClick,
-  totalConImpuestos,
-  openAdminModal,
+	project,
+	isCompleted,
+	onEdit,
+	onDelete,
+	handleCompleteClick,
+	totalConImpuestos,
+	openAdminModal,
 }) => {
-  const [showFullDesc, setShowFullDesc] = useState(false);
-  const { isLong, short } = useProjectDescription(project.description);
+	const [showFullDesc, setShowFullDesc] = useState(false);
+	const { isLong, short } = useProjectDescription(project.description);
 
-  return (
-    <article className="flex flex-col md:flex-row gap-12 p-6 md:p-8 justify-between">
-      <div className="flex flex-col gap-8 text-balance w-full ">
-        {/* Título y prioridad */}
-        <div className="flex flex-col-reverse md:flex-row justify-between mt-4 gap-12 md:gap-6">
-          <TitleProjectCard project={project} />
-          <div className="flex justify-end md:items-center gap-2">
-            <Priority project={project} />
-          </div>
-        </div>
+	return (
+		<article className="flex flex-col md:flex-row gap-12 p-6 md:p-8 justify-between">
+			{/* Contenido principal del proyecto */}
+			<div className="flex flex-col gap-6 justify-between">
+				<div className="flex flex-col gap-8 text-balance w-full ">
+					{/* Título y prioridad */}
+					<div className="flex flex-col-reverse md:flex-row justify-between mt-4 gap-12 md:gap-6">
+						<TitleProjectCard project={project} />
+						<div className="flex justify-end md:items-center gap-2">
+							<Priority project={project} />
+						</div>
+					</div>
 
-        <ProjectDescriptionInfoCard
-          project={project}
-          isLongDescription={isLong}
-          shortDesc={short}
-          showFullDesc={showFullDesc}
-          setShowFullDesc={setShowFullDesc}
-        />
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-            Servicios solicitados
-          </h3>
-          <ServicesProjectTag project={project} />
-        </div>
+					<ProjectDescriptionInfoCard
+						project={project}
+						isLongDescription={isLong}
+						shortDesc={short}
+						showFullDesc={showFullDesc}
+						setShowFullDesc={setShowFullDesc}
+					/>
+				</div>
+				<div className="flex flex-col gap-6">
+					{/* contador de tareas generadas */}
+					<div className="flex flex-col gap-2">
+						<h3 className="text-lg font-semibold text-gray-800 dark:text-white">
+							{project.tasks && project.tasks.length === 1 ? 'Tarea generada' : 'Tareas generadas'}
+						</h3>
+						<p className="text-gray-700 dark:text-gray-200">
+							{project.tasks && project.tasks.length === 1
+								? `1 Tarea generada`
+								: `${project.tasks ? project.tasks.length : 0} Tareas generadas`}
+						</p>
+					</div>
+					{/* mostrar nombre Tareas generadas*/}
+					<div className="flex flex-col gap-2">
+						<h3 className="text-lg font-semibold text-gray-800 dark:text-white">
+							Tareas generadas
+						</h3>
+						<p className="text-gray-700 dark:text-gray-200">
+							{project.tasks && project.tasks.length > 0 ? (
+								project.tasks.map((task, idx) => (
+									<span key={task.id || idx} className="block text-pretty">
+										{task.description || task.name || 'Sin descripción'}
+									</span>
+								))
+							) : (
+								<span>No hay tareas generadas</span>
+							)}
+						</p>
+					</div>
+				</div>
+			</div>
 
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-            Secciones solicitados
-          </h3>
-          <SectionsProjectTag project={project} />
-        </div>
+			{/* Fechas y Botones de Acción */}
+			<div className="text-lg md:text-base flex flex-col gap-8 justify-between h-full">
+				{/* Fechas y total del proyecto */}
+				<div className="flex flex-col gap-2 mt-4">
+					<ProjectCardDates project={project} isCompleted={isCompleted} />
+					<TotalCostProyectCard totalConImpuestos={totalConImpuestos} />
+				</div>
 
-        {/* contador de tareas generadas */}
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-            {project.tasks && project.tasks.length === 1
-              ? "Tarea generada"
-              : "Tareas generadas"}
-          </h3>
-          <p className="text-gray-700 dark:text-gray-200">
-            {project.tasks && project.tasks.length === 1
-              ? `1 Tarea generada`
-              : `${project.tasks ? project.tasks.length : 0} Tareas generadas`}
-          </p>
-        </div>
-        {/* mostrar nombre Tareas generadas*/}
-        <div className="flex flex-col gap-2">
-          <h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-            Tareas generadas
-          </h3>
-          <p className="text-gray-700 dark:text-gray-200">
-            {project.tasks && project.tasks.length > 0 ? (
-              project.tasks.map((task, idx) => (
-                <span key={task.id || idx} className="block text-pretty">
-                  {task.description || task.name || "Sin descripción"}
-                </span>
-              ))
-            ) : (
-              <span>No hay tareas generadas</span>
-            )}
-          </p>
-        </div>
-      </div>
-
-      {/* Datos del Proyecto */}
-      <div className="text-lg md:text-base">
-        {/* Fechas y total del proyecto */}
-        <div className="flex flex-col gap-2 mt-4">
-          <ProjectCardDates project={project} isCompleted={isCompleted} />
-          <TotalCostProyectCard totalConImpuestos={totalConImpuestos} />
-        </div>
-
-        {/* Botones de acción */}
-        <div className="flex flex-col gap-2 mt-12">
-          <ProjectActionButtons
-            isCompleted={isCompleted}
-            openAdminModal={openAdminModal}
-            onEdit={onEdit}
-            onDelete={onDelete}
-            handleCompleteClick={handleCompleteClick}
-          />
-          {isCompleted && (
-            <div className="flex flex-col gap-8">
-              <AdminActionButton onClick={openAdminModal} text="Ver Proyecto" />
-              <CloseProjectMessaje />
-            </div>
-          )}
-        </div>
-      </div>
-    </article>
-  );
+				{/* Botones de acción */}
+				<div className="flex flex-col gap-2 mt-12">
+					<ProjectActionButtons
+						isCompleted={isCompleted}
+						openAdminModal={openAdminModal}
+						onEdit={onEdit}
+						onDelete={onDelete}
+						handleCompleteClick={handleCompleteClick}
+					/>
+					{isCompleted && (
+						<div className="flex flex-col gap-8">
+							<AdminActionButton onClick={openAdminModal} text="Ver Proyecto" />
+							<CloseProjectMessaje />
+						</div>
+					)}
+				</div>
+			</div>
+		</article>
+	);
 };
 
 export default ContentProjectCard;
