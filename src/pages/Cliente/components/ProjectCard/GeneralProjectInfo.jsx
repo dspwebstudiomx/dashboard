f7fb9e9e337fb638cc2595b86d270acc0cc57a5e
Daniel Perez <dspwebstudio@gmail.com>
2025-05-29 12:38:10 -0600

    refactor: mejorar la estructura del modal en GeneralProjectInfo para mayor claridad

--- src/pages/Cliente/components/ProjectCard/GeneralProjectInfo.jsx
+++ src/pages/Cliente/components/ProjectCard/GeneralProjectInfo.jsx
@@ -32,17 +32,24 @@ const GeneralProjectInfo = ({
 	resetTaskForm,
 }) => {
 	return (
-		<Modal isOpen={isOpen} onClick={onClose} title="Administrar Proyecto">
+		<Modal
+			isOpen={isOpen}
+			onClick={onClose}
+			title={
+				<div className="flex flex-col sm:flex-row items-center justify-center gap-6">
+					<h2 id="titulo-proyecto" className="text-3xl text-center">
+						{project.title}
+					</h2>
+					<div className="flex flex-col md:flex-row gap-4 items-center justify-center">
+						<Priority project={project} />
+					</div>
+				</div>
+			}
+		>
 			<div
 				id="modal-content"
 				className="flex flex-col gap-8 pb-20 rounded-2xl border-2 border-gray-200 text-gray-800 bg-white p-8 shadow-lg dark:bg-gray-800 dark:text-gray-100"
 			>
-				<div className="flex flex-col md:flex-row gap-4 items-center justify-center mt-12 mb-8">
-					<h2 id="titulo-proyecto" className="text-3xl text-center">
-						{project.title}
-					</h2>
-					<Priority project={project} />
-				</div>
 				<div className="flex flex-col gap-6 py-12">
 					<h2 className="text-2xl font-semibold">Descripción del Proyecto</h2>
 					<span className="ml-4">
