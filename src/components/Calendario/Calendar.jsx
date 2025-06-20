0fb5f6062a5a43e9c151908c371a61933471bccc
Daniel Perez <dspwebstudio@gmail.com>
2025-06-20 12:16:37 -0600

    refactor: ajustar altura de secciones en el componente Calendar para mejorar el diseño

--- src/components/Calendario/Calendar.jsx
+++ src/components/Calendario/Calendar.jsx
@@ -51,7 +51,7 @@ const Calendar = () => {
 		const lastDayIndex = getDay(end);
 
 		return (
-			<section className="h-80">
+			<section className="h-full">
 				{/* Encabezado con iniciales de los días */}
 				<div className="grid grid-cols-7 gap-2 mb-2 border-b-2 border-gray-300 dark:border-gray-600 pb-2">
 					{weekDays.map((day, index) => (
@@ -89,7 +89,7 @@ const Calendar = () => {
 	};
 
 	return (
-		<div className="flex flex-col justify-center items-center h-64 rounded-lg mb-12">
+		<div className="flex flex-col justify-center items-center rounded-lg">
 			<div className="flex justify-between w-full px-4 items-center mb-4">
 				<button onClick={handlePrev} className="p-2">
 					<FaArrowLeft size={28} className="text-blue-600" />
