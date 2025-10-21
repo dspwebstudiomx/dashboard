f9f7831ad707ca8843e0965738f6c3d53c9350e2
Daniel Perez <dspwebstudio@gmail.com>
2025-10-21 13:16:53 -0600

    fix: actualizar campos y estilos en formularios y tablas de clientes para mejorar la presentación

--- src/pages/Cliente/components/CouponSection.jsx
+++ src/pages/Cliente/components/CouponSection.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Button from '@components/Botones/Button';
+import { BsFillTagFill } from 'react-icons/bs';
 
 const CouponSection = ({
 	coupon,
@@ -12,24 +13,27 @@ const CouponSection = ({
 	return (
 		<div className="flex flex-col gap-2 items-right justify-center">
 			<div id="coupon-section" className="flex flex-col md:flex-row gap-4 items-end justify-start">
-				<div className="flex gap-4 flex-col items-start justify-start">
-					<label className="text-xl text-gray-600 dark:text-gray-300 flex items-center gap-2 font-semibold mb-6">
+				<div className="flex flex-col items-start justify-start w-full">
+					<label className="text-gray-800 dark:text-gray-800 flex items-center gap-2 font-semibold mb-6 text-2xl">
+						<BsFillTagFill className="h-6 w-6 text-blue-700" />
 						Código de cupón
 					</label>
-					<div className="flex items-center justify-center">
-						<input
-							className="p-2 md:p-4 rounded border flex-1 h-12 md:w-[320px]"
-							type="text"
-							name="coupon"
-							value={coupon}
-							onChange={(e) => setcoupon(e.target.value)}
-							placeholder="Ingresa tu cupón"
-							onKeyDown={(e) => {
-								if (e.key === 'Enter') e.preventDefault();
-							}}
-						/>
+					<div className="flex items-center justify-between gap-4 w-full">
+						<div className="w-[320px]">
+							<input
+								className="p-2 md:p-4 border flex-1 h-12 md:w-[320px] rounded-xl"
+								type="text"
+								name="coupon"
+								value={coupon}
+								onChange={(e) => setcoupon(e.target.value)}
+								placeholder="Ingresa tu cupón"
+								onKeyDown={(e) => {
+									if (e.key === 'Enter') e.preventDefault();
+								}}
+							/>
+						</div>
 
-						<div className="flex items-center justify-end gap-2 mt-2 md:mt-0">
+						<div className="flex items-center justify-end gap-0 mt-2 md:mt-0">
 							<Button
 								variant="primary"
 								type="button"
