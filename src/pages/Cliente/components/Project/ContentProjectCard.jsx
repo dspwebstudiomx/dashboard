385d46d540922952e79cd98548894568dcdf6477
Daniel Perez <dspwebstudio@gmail.com>
2025-10-22 16:17:36 -0600

    fix: integrar componente ProjectCardTaskSummary para mejorar la gestión de tareas en ContentProjectCard

--- src/pages/Cliente/components/Project/ContentProjectCard.jsx
+++ src/pages/Cliente/components/Project/ContentProjectCard.jsx
@@ -14,6 +14,7 @@ import { useProjectDescription } from '@hooks/useProjectDescription';
 import ProjectActionButtons from '../ProjectActionButtons';
 import AdminActionButton from '../AdminActionButton';
 import Button from '@components/Botones/Button';
+import ProjectCardTaskSummary from '../ProjectCard/ProjectCardTaskSummary';
 
 const ContentProjectCard = ({ project, actions, netPayable }) => {
 	const [showFullDescriptionription, setshowFullDescriptionriptionription] = useState(false);
@@ -53,81 +54,13 @@ const ContentProjectCard = ({ project, actions, netPayable }) => {
 						setshowFullDescriptionription={setshowFullDescriptionriptionription}
 					/>
 				</div>
-				<div className="flex flex-col gap-6">
-					{/* mostrar nombre Tareas generadas*/}
-					<div className="flex flex-col justify-end gap-4 first-letter:uppercase text-sm h-64 overflow-y-auto">
-						<div className="flex gap-3 items-center">
-							<h3 className="text-lg font-semibold text-gray-800 dark:text-white">
-								Tareas generadas
-							</h3>
-							<p
-								className={`text-gray-100 border rounded-full w-7 h-7 flex items-center justify-center text-sm font-semibold ${
-									project.priority === 'Alta'
-										? 'bg-red-400 border-red-500'
-										: project.priority === 'Media'
-										? 'bg-yellow-400 border-yellow-500'
-										: 'bg-green-500 border-green-600'
-								}`}
-							>
-								{totalTasks.length}
-							</p>
-						</div>
-						<p className="text-gray-700 dark:text-gray-200 first-letter:uppercase text-base">
-							{paginatedTasks.length > 0 ? (
-								paginatedTasks
-									.sort((a, b) =>
-										a.status === 'Completado' ? 1 : b.status === 'Completado' ? -1 : 0
-									) // Ordena las tareas, colocando las completadas al final
-									.map((task, idx) => (
-										<span
-											key={task.id || idx}
-											className={`block text-pretty first-letter:uppercase ${
-												task.status === 'Completado' ? 'line-through text-gray-500' : ''
-											}`}
-										>
-											<MdKeyboardArrowRight
-												className={`inline mr-1 ${
-													task.priority === 'Alta'
-														? 'text-red-500'
-														: task.priority === 'Media'
-														? 'text-yellow-500'
-														: 'text-green-500'
-												}`}
-												size={24}
-											/>
-											{task.title || task.name || 'Sin descripción'} -
-											<span className="text-sm text-gray-600 dark:text-gray-400 ml-4 font-semibold">
-												{task.totalProgress}%
-											</span>
-										</span>
-									))
-							) : (
-								<span>No hay tareas generadas</span>
-							)}
-						</p>
-						{/* Paginación */}
-						{totalPages > 1 && (
-							<div className="flex gap-2 mt-4">
-								{currentPage > 1 && (
-									<Button
-										onClick={() => setCurrentPage((prev) => prev - 1)}
-										text="Anterior"
-										icon={MdKeyboardDoubleArrowLeft}
-										size="sm"
-									/>
-								)}
-								{currentPage < totalPages && (
-									<Button
-										onClick={() => setCurrentPage((prev) => prev + 1)}
-										icon={MdKeyboardDoubleArrowRight}
-										text="Siguiente"
-										size="sm"
-									/>
-								)}
-							</div>
-						)}
-					</div>
-				</div>
+
+				<ProjectCardTaskSummary
+					tasks={totalTasks}
+					totalPages={totalPages}
+					paginatedTasks={paginatedTasks}
+					setCurrentPage={setCurrentPage}
+				/>
 			</div>
 
 			{/* Fechas y Botones de Acción */}
