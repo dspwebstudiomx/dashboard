2f1b9b40340d7b141a7d992f646ecd850373b83c
Daniel Perez <dspwebstudio@gmail.com>
2025-12-09 08:48:03 -0600

    fix: mejorar la estructura y estilos del componente ClientsCard para una mejor presentación de la información del cliente

--- src/pages/ClientsPage/ClientsCard.jsx
+++ src/pages/ClientsPage/ClientsCard.jsx
@@ -68,128 +68,126 @@ const ClientsCard = ({ client, fetchClients, handleOpenModal }) => {
 	};
 
 	return (
-		<>
-			<article className="grid md:grid-cols-12 shadow-2xl rounded-2xl p-8 justify-center items-center md:items-start gap-5 bg-white dark:bg-gray-800 font-semibold mb-0 xl:h-auto last:md:mb-0 border-4 dark:border-gray-700 border-blue-400">
+		<article className="grid md:grid-cols-12 shadow-2xl rounded-2xl p-8 justify-center items-center md:items-start gap-5 bg-white dark:bg-gray-800 font-semibold mb-0 xl:h-auto last:md:mb-0 border-4 dark:border-gray-700 border-blue-400">
+			<div
+				id="client-logo"
+				className="col-span-12 md:col-span-12 items-center justify-between h-auto gap-12 my-auto"
+			>
 				<div
-					id="client-logo"
-					className="col-span-12 md:col-span-12 items-center justify-between h-auto gap-12 my-auto"
+					id="tarjeta-clients-imagen-nombreCompleto-redesSociales"
+					className="flex flex-col items-center justify-center gap-6"
 				>
+					<img
+						id="imagen-cliente"
+						src={
+							client.image
+								? `http://localhost:5000${client.image}`
+								: '../../../server/uploads/avatar_placeholder_large.png'
+						}
+						alt={client.fullName}
+						className="w-24 h-24 rounded-full mx-auto border-2 border-gray-300 object-cover bg-white"
+					/>
 					<div
-						id="tarjeta-clients-imagen-nombreCompleto-redesSociales"
-						className="flex flex-col items-center justify-center gap-6"
+						id="cliente-nombre-completo"
+						className="text-center flex flex-col items-center justify-center gap-0"
 					>
-						<img
-							id="imagen-cliente"
-							src={
-								client.image
-									? `http://localhost:5000${client.image}`
-									: '../../../server/uploads/avatar_placeholder_large.png'
-							}
-							alt={client.fullName}
-							className="w-24 h-24 rounded-full mx-auto border-2 border-gray-300 object-cover bg-white"
-						/>
-						<div
-							id="cliente-nombre-completo"
-							className="text-center flex flex-col items-center justify-center gap-0"
+						<h2 className="text-lg font-bold text-gray-700 dark:text-gray-100 text-center w-full m-4 flex flex-col md:flex-row gap-1 md:text-xl justify-center">
+							<span>{client?.fullName}</span>
+							<span>
+								{client?.lastName} {client?.lastName2}
+							</span>
+						</h2>
+						<Link
+							className="bg-blue-400 px-4 py-2 rounded-4xl text-blue-800"
+							to={`/clientes/${client?.id}`}
 						>
-							<h2 className="text-lg font-bold text-gray-700 dark:text-gray-100 text-center w-full mb-3 flex flex-col md:flex-row gap-1 justify-center">
-								<span>{client?.fullName}</span>
-								<span>
-									{client?.lastName} {client?.lastName2}
-								</span>
-							</h2>
-							<Link
-								className="bg-blue-400 px-4 py-2 rounded-4xl text-blue-800"
-								to={`/clientes/${client?.id}`}
+							{client?.id}
+						</Link>
+						<div className="col-span-12 flex flex-col md:flex-row items-center md:justify-between justify-center gap-6 xl:px-2 py-12">
+							<div
+								id="tarjeta-redes-sociales"
+								className="flex flex-wrap items-center gap-8 md:gap-4"
 							>
-								{client?.id}
-							</Link>
-							<div className="col-span-12 flex flex-col md:flex-row items-center md:justify-between justify-center gap-6 xl:px-2 py-12">
-								<div
-									id="tarjeta-redes-sociales"
-									className="flex flex-wrap items-center gap-8 md:gap-4"
-								>
-									{[
-										{
-											href: client?.website,
-											icon: <FaHome size={SocialStyles.iconSize} />,
-											title: 'Visitar sitio web',
-										},
-										{
-											href: `mailto:${client?.email}`,
-											icon: <FaEnvelope size={SocialStyles.iconSize} />,
-											title: 'Enviar correo electrónico',
-										},
-										{
-											href: `tel:${client?.phoneNumber}`,
-											icon: <FaPhone size={SocialStyles.iconSize} />,
-											title: 'Llamar',
-										},
-										{
-											href: client?.linkedin?.trim(),
-											icon: <FaLinkedin size={SocialStyles.iconSize} />,
-											title: 'Visitar LinkedIn',
-										},
-										{
-											href: client?.facebook?.trim(),
-											icon: <FaFacebook size={SocialStyles.iconSize} />,
-											title: 'Visitar Facebook',
-										},
-										{
-											href: client?.instagram?.trim(),
-											icon: <FaInstagram size={SocialStyles.iconSize} />,
-											title: 'Visitar Instagram',
-										},
-										{
-											href: client?.twitter?.trim(),
-											icon: <FaXTwitter size={SocialStyles.iconSize} />,
-											title: 'Visitar Twitter',
-										},
-									]
-										.filter((social) => social.href)
-										.map((social, index) => (
-											<a
-												key={index}
-												href={social.href}
-												target="_blank"
-												rel="noopener noreferrer"
-												className={SocialStyles.link}
-												aria-label={social.title}
-												title={social.title}
-											>
-												{social.icon}
-											</a>
-										))}
-								</div>
+								{[
+									{
+										href: client?.website,
+										icon: <FaHome size={SocialStyles.iconSize} />,
+										title: 'Visitar sitio web',
+									},
+									{
+										href: `mailto:${client?.email}`,
+										icon: <FaEnvelope size={SocialStyles.iconSize} />,
+										title: 'Enviar correo electrónico',
+									},
+									{
+										href: `tel:${client?.phoneNumber}`,
+										icon: <FaPhone size={SocialStyles.iconSize} />,
+										title: 'Llamar',
+									},
+									{
+										href: client?.linkedin?.trim(),
+										icon: <FaLinkedin size={SocialStyles.iconSize} />,
+										title: 'Visitar LinkedIn',
+									},
+									{
+										href: client?.facebook?.trim(),
+										icon: <FaFacebook size={SocialStyles.iconSize} />,
+										title: 'Visitar Facebook',
+									},
+									{
+										href: client?.instagram?.trim(),
+										icon: <FaInstagram size={SocialStyles.iconSize} />,
+										title: 'Visitar Instagram',
+									},
+									{
+										href: client?.twitter?.trim(),
+										icon: <FaXTwitter size={SocialStyles.iconSize} />,
+										title: 'Visitar Twitter',
+									},
+								]
+									.filter((social) => social.href)
+									.map((social, index) => (
+										<a
+											key={index}
+											href={social.href}
+											target="_blank"
+											rel="noopener noreferrer"
+											className={SocialStyles.link}
+											aria-label={social.title}
+											title={social.title}
+										>
+											{social.icon}
+										</a>
+									))}
 							</div>
 						</div>
 					</div>
 				</div>
-				<div id="client-data" className="col-span-12 text-sm xl:text-base border-collapse h-full">
-					<div
-						id="redes-sociales-botones-edicion"
-						className="flex gap-4 justify-between items-center w-full"
-					>
-						<EditClientButton client={client} handleOpenModal={handleOpenModal} />
-						<div className="flex gap-4">
-							<button
-								onClick={openTableModal}
-								className=" rounded-lg flex items-center justify-center"
-							>
-								<FaEye size={34} className="inline mr-2 text-blue-700  hover:text-blue-500" />
-							</button>
-							<button
-								id="eliminar-cliente"
-								onClick={openConfirmModal}
-								className="text-red-500 hover:text-red-700"
-								aria-label={`Eliminar cliente ${client?.fullName}`}
-							>
-								<FaTrash size={28} />
-							</button>
-						</div>
+			</div>
+			<div id="client-data" className="col-span-12 text-sm xl:text-base border-collapse h-full">
+				<div
+					id="redes-sociales-botones-edicion"
+					className="flex gap-4 justify-between items-center w-full"
+				>
+					<EditClientButton client={client} handleOpenModal={handleOpenModal} />
+					<div className="flex gap-4">
+						<button
+							onClick={openTableModal}
+							className=" rounded-lg flex items-center justify-center"
+						>
+							<FaEye size={34} className="inline mr-2 text-blue-700  hover:text-blue-500" />
+						</button>
+						<button
+							id="eliminar-cliente"
+							onClick={openConfirmModal}
+							className="text-red-500 hover:text-red-700"
+							aria-label={`Eliminar cliente ${client?.fullName}`}
+						>
+							<FaTrash size={28} />
+						</button>
 					</div>
 				</div>
-			</article>
+			</div>
 
 			{isConfirmModalOpen && (
 				<div className="fixed inset-0 flex items-center justify-center bg-black/45 opacity-100 z-50">
@@ -225,7 +223,7 @@ const ClientsCard = ({ client, fetchClients, handleOpenModal }) => {
 				isTableModalOpen={isTableModalOpen}
 				closeTableModal={closeTableModal}
 			/>
-		</>
+		</article>
 	);
 };
 
